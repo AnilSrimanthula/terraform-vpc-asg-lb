ea46d29a87205b74dd020d321631731f8e95f80a
AnilSrimanthula <anilsrimanthula1999@gmail.com>
2025-10-29 13:45:29 +0530

    Create sg.tf

---
+++ sg.tf
@@ -0,0 +1,17 @@
+resource "aws_security_group" "mysg" {
+name = "t-sg"
+description = "it has all traffic"
+vpc_id = aws_vpc.myvpc.id
+ingress {
+from_port = 0
+to_port = 0
+cidr_blocks = ["0.0.0.0/0"]
+protocol = "-1"
+}
+egress {
+from_port = 0
+to_port = 0
+cidr_blocks = ["0.0.0.0/0"]
+protocol = "-1"
+}
+}
